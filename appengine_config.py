53180b924c747d720724487c3472abf99d2f9924
Georgi Tenev <chipsanbg@gmail.com>
2014-10-19 17:01:55 +0100

    cloned the Flask boilerplate and created the secret keys. Updated the .gitignore so the keys are not in the version control.

--- appengine_config.py
+++ appengine_config.py
@@ -1,6 +1,17 @@
-"""`appengine_config` gets loaded when starting a new application instance."""
-import vendor
-# insert `lib` as a site directory so our `main` module can load
-# third-party libraries, and override built-ins with newer
-# versions.
-vendor.add('lib')
+"""
+App Engine config
+
+"""
+
+
+def gae_mini_profiler_should_profile_production():
+    """Uncomment the first two lines to enable GAE Mini Profiler on production for admin accounts"""
+    # from google.appengine.api import users
+    # return users.is_current_user_admin()
+    return False
+
+
+def webapp_add_wsgi_middleware(app):
+    from google.appengine.ext.appstats import recording
+    app = recording.appstats_wsgi_middleware(app)
+    return app
