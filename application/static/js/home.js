d79bb41ba4bd79363988bb5e7d6bcb06baa271ef
Denys Demchenko <dem.denn@gmail.com>
2014-11-04 21:37:32 +0000

    Added navigation using text field and button

--- application/static/js/home.js
+++ application/static/js/home.js
@@ -1,37 +1,76 @@
  
+// global variables
+var DEFAULT_LOCATION = "England";
+var DEFAULT_ZOOM = 6;
 
-/* Your custom JavaScript here */
-
+var map;
 
 
 // Zooms the map in to the <locationName>
 // (takes location (e.g Southampton)  and Zoom Level (e.g 8).)
-(function(locationName,zoomLevel) {
+
+
+
+var init = function(locationName,zoomLevel) {
  	//zoomLevel = 8;
 	//var locationName = "Cambrige";
-	var geoCodingAPI_URL = "http://localhost:8080/api/2/";
-	
-	$.getJSON( geoCodingAPI_URL+locationName).success(function( json ) {
+	if (!map) {
+		var geoCodingAPI_URL = "http://localhost:8080/api/2/";
+		$.getJSON( geoCodingAPI_URL+locationName).success(function( json ) {
+		// convert name to coordinates using /api/2/
+			var centerLocation = L.latLng(json["results"][0]["geometry"]["location"]["lat"],
+      								 	  json["results"][0]["geometry"]["location"]["lng"]);
+			// Sitting bounds
+      		var northEast = L.latLng(json["results"][0]["geometry"]["bounds"]["northeast"]["lat"],
+      		  					   	 json["results"][0]["geometry"]["bounds"]["northeast"]["lng"]);
+      		var southWest = L.latLng(json["results"][0]["geometry"]["bounds"]["southwest"]["lat"],
+      		 				      	 json["results"][0]["geometry"]["bounds"]["southwest"]["lng"]);
+      		bounds = L.latLngBounds(southWest, northEast);
 
+        	map = L.map('map',{
+ 	 			center: centerLocation,
+ 				zoom: zoomLevel,
+ 				//maxBounds: bounds
+ 		});
+	var tiles = L.tileLayer('http://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', 
+	 {
+       	id: 'qwerty123123.k312hg0n'
+     }).addTo(map);
+ 	
+ 	   });
+    }
+}(DEFAULT_LOCATION,DEFAULT_ZOOM);
+
+
+
+var zoomTo = function(locationName,zoomLevel) {
+	var geoCodingAPI_URL = "http://localhost:8080/api/2/";
+	$.getJSON( geoCodingAPI_URL+locationName).success(function( json ) {
+		// convert name to coordinates using /api/2/
+		var centerLocation = L.latLng(json["results"][0]["geometry"]["location"]["lat"],
+      							   json["results"][0]["geometry"]["location"]["lng"]);
+		// Bounds
       	var northEast = L.latLng(json["results"][0]["geometry"]["bounds"]["northeast"]["lat"],
       							   json["results"][0]["geometry"]["bounds"]["northeast"]["lng"]);
       	var southWest = L.latLng(json["results"][0]["geometry"]["bounds"]["southwest"]["lat"],
       						       json["results"][0]["geometry"]["bounds"]["southwest"]["lng"]);
-      	bounds = L.latLngBounds(southWest, northEast);
+      	//bounds = L.latLngBounds(southWest, northEast);
 
-      	var map = L.map('map',{
- 	 		center: [52, -1.5],
- 			zoom: zoomLevel,
- 			maxBounds: bounds
- 		});
-
- 		var tiles = L.tileLayer('http://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', 
-	 	{
-        	id: 'qwerty123123.k312hg0n'
-     	}).addTo(map);
-
+      	// Map not inialized
+      	if (!map) {
+         	init(DEFAULT_LOCATION,DEFAULT_ZOOM);
+ 		}else{ 		 			
+ 			// Map initialised	 			
+ 			//map.setView(centerLocation);
+ 			map.setZoom(zoomLevel);
+ 			map.panTo(centerLocation);
+ 			
+ 			//map.setMaxBounds(bounds);
+		
+			
+ 		}
     });
-})();
+};
 
 /*
  var southWest = L.latLng(49.8669688, -8.649357199999999),
