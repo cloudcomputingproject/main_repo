96acb11ad81d613b874713208867dc3afffa213e
Denys Demchenko <dem.denn@gmail.com>
2014-11-04 15:08:56 +0000

    Geocoding API implemented

--- application/api/external_api.py
+++ application/api/external_api.py
@@ -21,6 +21,7 @@ from application.models import ExampleModel
 from application import app
 
 from servers import worldbank
+from servers import geocoding
 
 # Flask-Cache (configured to use App Engine Memcache API)
 cache = Cache(app)
@@ -31,13 +32,14 @@ external_api = Blueprint('external_api', __name__)
 to use the api you need to specify the external server that is being used and also 
 pass the necessery parameters.
 example request to the worldbank's api
-localhost:8000/api/1/NY.GDP.MKTP.CD/date=2013/format=json
+localhost:8080/api/1/NY.GDP.MKTP.CD/date=2013/format=json
 
 supported external servers, our corresponding method and the parameters it takes
 1 worldbank - getWorldBankData()  params: dataType?date&format 
 '''
 servers = {
-'worldbank' : 1
+'worldbank' : 1,
+'geocoding' : 2 
 }
 
 @external_api.route('/api/'+str(servers['worldbank'])+'/<dataType>/<date>/<format>')
@@ -48,5 +50,20 @@ def getWorldBankData(dataType, date, format):
 
  	return jsonify(result)
 
- 
+
+# Converting addresses into geographic coordinates
+@external_api.route('/api/'+str(servers['geocoding'])+'/<address>')
+def getGeocodingData(address):
+	result = geocoding.getData(address)
+ 	return jsonify(result)
+
+
+
+
+
+
+
+
+
+
 
---
+++ application/api/servers/geocoding.py
@@ -0,0 +1,50 @@
+"""
+handles interaction with the worldbank api
+
+"""
+import urllib2
+import json
+
+from google.appengine.api import users
+from google.appengine.runtime.apiproxy_errors import CapabilityDisabledError
+
+from flask import request, render_template, flash, url_for, redirect, Blueprint
+
+from flask_cache import Cache
+
+
+
+from application.decorators import login_required, admin_required
+from application.forms import ExampleForm
+from application.models import ExampleModel
+
+from application import app
+
+# Flask-Cache (configured to use App Engine Memcache API)
+cache = Cache(app)
+
+'''
+ API Key for geocoding can be found here:
+	https://console.developers.google.com/project/cobalt-mantis-732/apiui/credential
+
+Example link (need to edit api key): 
+	https://maps.googleapis.com/maps/api/geocode/json?address=122+Flinders+St,+Darlinghurst,+NSW,+Australia&sensor=false&key={key}
+
+'''
+
+API_KEY = "AIzaSyAXZycyWt-ZGGooaAycfCyfZuV1W5uKBGg"
+
+components = "components=country:UK"
+def getData(address):
+	url = "https://maps.googleapis.com/maps/api/geocode/json?address="+address+"&"+components+"&"+"sensor=false"+"&"+"key="+API_KEY
+	request = urllib2.urlopen(url)	
+	result = json.load(request)
+	
+	return result
+
+
+
+
+
+
+
