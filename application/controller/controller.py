ef89d7a8017490f9f70f8029fcd80fccffbe0699
Ruben <rana2g13@soton.ac.uk>
2014-11-18 17:17:55 +0000

    New json parsing

--- application/controller/controller.py
+++ application/controller/controller.py
@@ -12,17 +12,25 @@ from application.forms import ExampleForm
 from application import app
 
 from servers import police
+<<<<<<< HEAD
+
+#List of all the features implemented, the structure is a dictionary, {JsonKeyword, functionName}
+featuresOptions = {"police" : processPolice,
+					"weather" : processWeather,
+					"restaurants" : processRestaurants}
+=======
 from servers import geocoding
 
 def getGeoCoding(location):
 	name = location["name"]
 	print name
 	return geocoding.getData(name).read()
+>>>>>>> 39bf24520d2c7ac16927117a7f3e280fc3a72489
 #takes python object representation fo the received JSON object
 def main(data):
 	location = data["location"]
 	if location:
-		processLocation(location)
+		actualLocation = processLocation(location)
 	features = data["features"]
 	if features:
 		processFeatures(features)
@@ -31,17 +39,39 @@ def main(data):
 	#with some external API
 	#we parse that response with the PARSER
 	#and return it to the request handler
+	print actualLocation.locationName
+	print "Latitude: " + str(actualLocation.southWest[0]) + ", Longitude: " + str(actualLocation.southWest[1])
+	print actualLocation.northEast
 	test_response = police.getCategories()
 	
 	temp = test_response.read()
 
 	return temp
+
 def processLocation(location):
-	#TODO
-	return 
-#takes array fo features
+	nEast = location["bounds"]["NorthEast"]
+	sWest = location["bounds"]["SouthWest"]
+	place = location["name"]
+	return Boundaries(nEast, sWest, place	)
+
+#takes array of features
 def processFeatures(features):
 	for feature in features:
-		#TODO
+		featuresOptions[feature["name"]](feature["args"])
 		print feature['name']
 
+def processPolice(policeArgs):
+	print policeArgs
+
+def processWeather(policeArgs):
+	print policeArgs
+
+def processRestaurants(policeArgs):
+	print policeArgs
+
+# Container class, it contains a location given by leaflet
+class Boundaries:
+	def __init__(self, northEast=[0,0], southWest=[0,0], locationName="undefined"):
+		self.northEast = northEast
+		self.southWest = southWest
+		self.locationName = locationName
