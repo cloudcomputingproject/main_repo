1e2c3b774b0c53704d9c720f8c2f53777dfbaa9a
Ruben <rana2g13@soton.ac.uk>
2014-11-18 17:06:18 +0000

    Started object parsing

--- application/controller/controller.py
+++ application/controller/controller.py
@@ -12,11 +12,16 @@ from application.forms import ExampleForm
 from application import app
 
 from servers import police
+
+#List of all the features implemented, the structure is a dictionary, {JsonKeyword, functionName}
+featuresOptions = {"police" : processPolice,
+					"weather" : processWeather,
+					"restaurants" : processRestaurants}
 #takes python object representation fo the received JSON object
 def main(data):
 	location = data["location"]
 	if location:
-		processLocation(location)
+		actualLocation = processLocation(location)
 	features = data["features"]
 	if features:
 		processFeatures(features)
@@ -25,17 +30,39 @@ def main(data):
 	#with some external API
 	#we parse that response with the PARSER
 	#and return it to the request handler
+	print actualLocation.locationName
+	print "Latitude: " + str(actualLocation.southWest[0]) + ", Longitude: " + str(actualLocation.southWest[1])
+	print actualLocation.northEast
 	test_response = police.getCategories()
 
 	temp = test_response.read()
 
 	return temp
+
 def processLocation(location):
-	#TODO
-	return 
-#takes array fo features
+	nEast = location["bounds"]["NorthEast"]
+	sWest = location["bounds"]["SouthWest"]
+	place = location["name"]
+	return Boundaries(nEast, sWest, place	)
+
+#takes array of features
 def processFeatures(features):
 	for feature in features:
-		#TODO
+		featuresOptions[feature["name"]](feature["args"])
 		print feature['name']
 
+def processPolice(policeArgs):
+	print policeArgs
+
+def processWeather(policeArgs):
+	print policeArgs
+
+def processRestaurants(policeArgs):
+	print policeArgs
+
+# Container class, it contains a location given by leaflet
+class Boundaries:
+	def __init__(self, northEast=[0,0], southWest=[0,0], locationName="undefined"):
+		self.northEast = northEast
+		self.southWest = southWest
+		self.locationName = locationName
