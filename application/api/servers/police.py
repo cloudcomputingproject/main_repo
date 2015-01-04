351718ac36a49e8fac94c785d272afeccf2f8cac
Ruben <rana2g13@soton.ac.uk>
2015-01-04 19:16:11 +0000

    Removed comments and unused code
    Set timeouts for urlopen calls
    
    Created a function that allows for the police API to get data from places where the number of crimes is bigger than 10,000

--- application/api/servers/police.py
+++ application/api/servers/police.py
@@ -10,12 +10,9 @@ from flask import request, render_template, flash, url_for, redirect, Blueprint,
 
 from flask_cache import Cache
 
-
-
 from application.decorators import login_required, admin_required
 from application.forms import ExampleForm
 from application.models import ExampleModel
-
 from application import app
 
 url = 'http://data.police.uk/api/'
@@ -24,8 +21,9 @@ url = 'http://data.police.uk/api/'
 def getData():
 	global url
 	#json object
+	
 	try:
-		data = urllib2.urlopen(url, timeout = 20)
+		data = urllib2.urlopen(url, timeout = 50)
 	except urllib2.URLError, e:
 		url = 'http://data.police.uk/api/'
 		raise NodeExcessException("The nodes returned by your query exceed the 10,000 limit")
@@ -46,7 +44,25 @@ def getCategories():
 
 	url += 'crime-categories?date='+lastUpdated()
 	return getData()
-			
+
+def combineResult(resultsArray):
+	result = ''
+	for r in resultsArray:
+		result += r[1:-1] + ','
+	return result
+
+#Divides a square in 4 interior squares
+#the format of the output will be:
+# [[sq1Lat, sq1Lng],[sq2Lat, sq2Lng],[sq3Lat, sq3Lng],[sq4Lat, sq4Lng]]			
+def getDivision(latArr, lngArr):
+	midLat = (latArr[0] + latArr[1])/2
+	midLng = (lngArr[0] + lngArr[2])/2
+	sq1 = [[latArr[0], midLat, midLat, latArr[0]],[lngArr[0], lngArr[0], midLng, midLng]]
+	sq2 = [[latArr[1], midLat, midLat, latArr[1]],[lngArr[1], lngArr[1], midLng, midLng]]
+	sq3 = [[latArr[2], midLat, midLat, latArr[2]],[lngArr[2], lngArr[2], midLng, midLng]]
+	sq4 = [[latArr[3], midLat, midLat, latArr[3]],[lngArr[3], lngArr[3], midLng, midLng]]
+	return [sq1, sq2, sq3, sq4]
+
 #date must be in the format yyyy-mm
 def getCrimesInAreaData(category, latArr, lngArr, date):
 	global url
@@ -59,7 +75,20 @@ def getCrimesInAreaData(category, latArr, lngArr, date):
 				url+=str(latArr[j]) + ',' + str(lngArr[j]) + ':'
 			url = url[:-1]
 			url+='&date=' + date
-			result += getData()[1:-1] + ','
+			try:
+				result += getData()[1:-1] + ','
+			except NodeExcessException as e:
+				if len(latArr) > 4 or len(lngArr) > 4:
+					raise
+				divisionResults = []
+				squareDivisions = getDivision(latArr, lngArr)
+				divisionResults.append(getCrimesInAreaData(category, squareDivisions[0][0], squareDivisions[0][1], date))
+				divisionResults.append(getCrimesInAreaData(category, squareDivisions[1][0], squareDivisions[1][1], date))
+				divisionResults.append(getCrimesInAreaData(category, squareDivisions[2][0], squareDivisions[2][1], date))
+				divisionResults.append(getCrimesInAreaData(category, squareDivisions[3][0], squareDivisions[3][1], date))
+				aux = combineResult(divisionResults)
+				if aux is not '':
+					result += combineResult(divisionResults)
 			if cat is 'all-crime':
 				break
 
